e2e08bea03ed77f4b0d5359405cdedb63e5e0e0f
Héctor Acosta <hector_ag1992@hotmail.com>
2017-08-15 19:56:36 +0100

    feat(category): Added basic implementation for category model

--- src/AboutYou/Entity/Category.php
+++ src/AboutYou/Entity/Category.php
@@ -2,7 +2,6 @@
 
 namespace AboutYou\Entity;
 
-
 class Category
 {
     /**
@@ -25,4 +24,30 @@ class Category
      * @var \AboutYou\Entity\Product[]
      */
     private $products = [];
+
+    public function __construct(int $id, String $name, array $products = []){
+        $this->id = $id;
+        $this->name = $name;
+        $this->products = $products;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName(String $name){
+        $this->name = $name;
+    }
+
+    public function addProduct(Product $product){
+        $this->products[] = $product;
+    }
+
+    public function getProducts(){
+        return $this->products;
+    }
 }
