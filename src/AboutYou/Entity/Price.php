358b9248a0aaf01fa3013c552184d69a1f08e56e
Héctor Acosta <hector_ag1992@hotmail.com>
2017-08-15 19:56:59 +0100

    feat(price): Added basic implementation for price model

--- src/AboutYou/Entity/Price.php
+++ src/AboutYou/Entity/Price.php
@@ -2,7 +2,6 @@
 
 namespace AboutYou\Entity;
 
-
 class Price
 {
     /**
@@ -32,4 +31,35 @@ class Price
      * @var \AboutYou\Entity\Variant
      */
     private $variant;
+
+    public function __construct(int $current, ?int $old, bool $isSale, Variant $variant){
+        $this->current = $current;
+        $this->old = $old;
+        $this->isSale = $isSale;
+        $this->variant = $variant;
+    }
+
+    public function getCurrent(){
+        return $this->current;
+    }
+
+    public function setCurrent(int $current){
+        $this->current = $current;
+    }
+
+    public function getOld(){
+        return $this->old;
+    }
+
+    public function setOld(int $old){
+        $this->old = $old;
+    }
+
+    public function getIsSale(){
+        return $this->isSale;
+    }
+
+    public function setIsSale(bool $isSale){
+        $this->isSale = sale;
+    }
 }
